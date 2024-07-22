39281661e4e09a65effbd37e8fd7daa0264699e9
Joydip <joydip.mondal@gmail.com>
2024-07-22 15:07:47 +0530

    task created

---
+++ codes/day-1/js-features-app/index.js
@@ -0,0 +1,6 @@
+let choiceStr = prompt('choice', '0')
+console.log(typeof choiceStr);
+//const choice = Number(choiceStr)
+//console.log(choice);
+//const choice = parseInt(choiceStr)
+//const choice = parseFloat(choiceStr)
