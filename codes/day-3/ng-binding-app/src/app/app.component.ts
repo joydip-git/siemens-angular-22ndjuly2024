a292485b169dd854d4c3b499dedb686a8eb5ea8d
Joydip <joydip.mondal@gmail.com>
2024-07-24 12:31:25 +0530

    binding app updated

--- codes/day-3/ng-binding-app/src/app/app.component.ts
+++ codes/day-3/ng-binding-app/src/app/app.component.ts
@@ -12,6 +12,10 @@ export class AppComponent {
   alternateButtonClsName = "button-alternate-style"
   showAlternate = false
   showMessage = false
+  selectionChoice = 0
+  firstValue = 0
+  secondValue = 0
+  resultValue = 0
 
   increaseWidth(newWidth: number) {
     this.headerWidth = newWidth
@@ -22,4 +26,38 @@ export class AppComponent {
   changeShowMessage() {
     this.showMessage = !this.showMessage
   }
+
+  calculate() {
+    switch (this.selectionChoice) {
+      case 1:
+        this.resultValue = this.firstValue + this.secondValue
+        break;
+      case 2:
+        this.resultValue = this.firstValue - this.secondValue
+        break;
+      case 3:
+        this.resultValue = this.firstValue * this.secondValue
+        break;
+      case 4:
+        this.resultValue = this.firstValue / this.secondValue
+        break;
+
+      default:
+        break;
+    }
+  }
+  updateChoice(choice: number) {
+    this.selectionChoice = choice
+  }
+  updateFirst(val: number) {
+    this.firstValue = val
+  }
+  updateSecond(val: number) {
+    this.secondValue = val
+  }
+  // updateSecond(e: any) {
+  //   console.log(e.data);
+  //   const inputElement = e.target as HTMLInputElement
+  //   console.log(+inputElement.value);
+  // }
 }
